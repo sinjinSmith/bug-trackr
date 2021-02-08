827bdefbe75f36f3716116c7ead57d2b94ef549a
Sinjin Smith <53022554+sinjinSmith@users.noreply.github.com>
2021-02-08 07:22:30 -0700

    Delete Bug-Trackr directory

--- Bug-Trackr/utils.py
+++
@@ -1,9 +0,0 @@
-from main import dbName
-from flask import session
-
-def add_user(name, password, email):
-    return dbName(name, password, email)
-
-def clear_session(session_var):
-    for data in session_var:
-        session.pop(data, None)
